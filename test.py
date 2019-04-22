c23eb18fb67610b0491bef8f4b4c7d751032ffcc
xshine_runningpig <hengguangliu@foxmail.com>
2019-04-22 16:35:34 +0800

    txt processing test

---
+++ test.py
@@ -0,0 +1,23 @@
+with open('./test', 'r') as f:
+    for line in f:
+        print(line)
+
+
+# ========================== #
+# matching using spacy lib
+# ========================== #
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+# Add match ID "HelloWorld" with no callback and one pattern
+pattern = [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}]
+matcher.add("HelloWorld", None, pattern)
+
+doc = nlp(u"Hello, world! Hello world!")
+matches = matcher(doc)
+for match_id, start, end in matches:
+    string_id = nlp.vocab.strings[match_id]  # Get string representation
+    span = doc[start:end]  # The matched span
+    print(match_id, string_id, start, end, span.text)
