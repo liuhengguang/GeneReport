488aba9e40aec2449826f5e0e0835f3e80bd7716
xshine_runningpig <hengguangliu@foxmail.com>
2019-05-08 09:54:37 +0800

    v2

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from openpyxl import load_workbook
 from advice_db.advice_db import *
 
 
-def xlsx_read(file_dir=Params.example_file, sheet_name="男（799）"):
+def xlsx_read(file_dir=Params.example_file, sheet_name="男（799）", min_col=0, max_col=14, min_row=2, max_row=75):
     wb = load_workbook(file_dir)
     sheet = wb[sheet_name]
     # print("===================表格基本信息：=======================")
     # print("sheet names:", wb.sheetnames)
-    # print("男（799）的维度：", sheet.calculate_dimension())
-    data = sheet['A:N']
+    print(sheet_name+"的维度：", sheet.calculate_dimension())
+    r = sheet.calculate_dimension()
+    data = sheet[r.split(":")[0][0]+":"+r.split(":")[1][0]]
+    if max_row >= int(r.split(":")[1][1:]):
+        max_row = int(r.split(":")[1][1:])
     total = []
-    for col in range(0, 14):  # 列
+    for col in range(min_col, max_col):  # 列
         temp = []
-        for row in range(2, 75):  # 行
+        for row in range(min_row, max_row):  # 行
             # print(data[row][col].value)
             temp.append(data[col][row].value)
         # print(temp)
@@ -25,16 +28,16 @@ def xlsx_read(file_dir=Params.example_file, sheet_name="男（799）"):
 # ======================================== #
 # main
 # ======================================== #
-total_cols = xlsx_read(sheet_name="男（799）")
+input_cols = xlsx_read(sheet_name="男（799）")
 # a exp line in total_rows: (1, '骨科疾病', '骨关节炎', 0.609499828501506, 8.868, 14.5497, 2, None)]
-total_rows = list(zip(total_cols[0],
-                      total_cols[1],
-                      total_cols[2],
-                      total_cols[6],
-                      total_cols[7],
-                      total_cols[9],
-                      total_cols[12],
-                      total_cols[13]
+input_rows = list(zip(input_cols[0],
+                      input_cols[1],
+                      input_cols[2],
+                      input_cols[6],
+                      input_cols[7],
+                      input_cols[9],
+                      input_cols[12],
+                      input_cols[13]
                       ))
 
 # ================= #
@@ -51,26 +54,28 @@ total_rows = list(zip(total_cols[0],
 骨科疾病
 
 """
-jibing_high_p = [i for i in total_rows
+jibing_high_p = [i for i in input_rows
                  if i[0] == 1 and i[4] >= 10]
-jibing_high_population = [i for i in total_rows
+jibing_high_population = [i for i in input_rows
                           if i[0] == 1 and i[3] >= 2 and i[4] <= 10]
 jibing = jibing_high_p + jibing_high_population
 
-kuangwz = [i for i in total_rows
+kuangwz = [i for i in input_rows
            if i[1] == "矿物质" and "高" in i[-1]]
 
-weiss = [i[2] for i in total_rows
+weiss = [i[2] for i in input_rows
          if i[1] == "维生素" and i[2] != "叶酸" and "高" in i[-1]]
 
-yesuan = [i[2] for i in total_rows
+yesuan = [i[2] for i in input_rows
           if i[2] == "叶酸" and "高" in i[-1]]
 
-shansdx = [i for i in total_rows
+shansdx = [i for i in input_rows
            if i[1] == "膳食代谢" and ("高" in i[-1] or "浅尝" in i[-1] or "戒酒" in i[-1] or "小酌" in i[-1] or "不耐受" in i[-1])]
 
-personal_features = [i for i in total_rows
+personal_features = [i for i in input_rows
                      if i[0] == 3]
+
+"""
 # ========================= #
 # step2: advices
 # ========================= #
@@ -143,6 +148,75 @@ personal_sport_features = []
 for i in personal_features:
     if i[1] == "运动能力":
         personal_sport_features.append(i)
+"""
+
+"""
+# ====================== #
+# knowledge
+# ====================== #
+knowledge_cols = xlsx_read(file_dir=Params.advices_relation, sheet_name="man_799", max_col=3, min_row=1, max_row=1000)
+knowledge_rows = list(zip(knowledge_cols[0], knowledge_cols[1]))
+for row in knowledge_rows:
+    if row[0] == 0 and ("烟" not in row[1] and "酒" not in row[1]):
+        print(row)
+
+for row in knowledge_rows:
+    if row[0] == 1:
+        print(row)
+
+for row in knowledge_rows:
+    if row[0] == 2:
+        print(row)
+"""
+
+advices_cols = xlsx_read(Params.man_and_woman_799_advices, sheet_name="man_799_vn", max_col=3, min_row=1, max_row=1000)
+knowledge_rows = list(zip(advices_cols[1], advices_cols[2]))
+cnt = 0
+cnt2 = 0
+diseases = []
+diseases2 = []
+original_advices = dict()
+splited_advices = dict()
+label = False
+label2 = False
+if knowledge_rows[-1] != ("end", "end"):
+    knowledge_rows.append(("end", "end"))
+for row in knowledge_rows:
+    d = row[0]
+    if "#" in d:
+        temp = []
+        label = True
+        cnt = cnt + 1
+        d = d[1:]
+        d = d.replace(" ", "")
+        diseases.append(d)
+        # print(d)
+    if label and "分词结果" in row[0]:
+        print(diseases[cnt-1])
+        label = False
+        original_advices[diseases[cnt-1]] = temp
+    if label and "(" in row[0]:
+        temp.append(row[0])
+
+    if "分词结果" in d:
+        temp2 = []
+        label2 = True
+        cnt2 = cnt2 + 1
+        continue
+        # print(d)
+    if label2 and ("#" in row[0] or "end" in row[0]):
+        print(diseases[cnt-2])
+        label2 = False
+        splited_advices[diseases[cnt-2]] = temp2
+    if label2:
+        temp2.append(row[0])
+
+
+# advices
+for row in jibing:
+    print(row[2], ":")
+    for advice in splited_advices[row[2]]:
+        print("    "+advice)
 
 
 
--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ import os
 class Params:
     root_dir = pkg_resources.resource_filename(__name__, ".")
     example_file = os.path.join(root_dir, "./test/gene_report_20190426.xlsx")
+    advices_relation = os.path.join(root_dir, "./test/拆分后各项建议关联分析.xlsx")
+    man_and_woman_799_advices = os.path.join(root_dir, "./test/man_and_woman_799_advices.xlsx")
+
