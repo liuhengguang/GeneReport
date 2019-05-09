a5c4e20cb0d4bbf53f48436510ce61bb7bcc5f59
xshine_runningpig <hengguangliu@foxmail.com>
2019-05-09 10:46:19 +0800

    v3

--- main.py
+++ main.py
@@ -25,60 +25,56 @@ def xlsx_read(file_dir=Params.example_file, sheet_name="男（799）", min_col=0
     return total
 
 
-# ======================================== #
-# main
-# ======================================== #
-input_cols = xlsx_read(sheet_name="男（799）")
-# a exp line in total_rows: (1, '骨科疾病', '骨关节炎', 0.609499828501506, 8.868, 14.5497, 2, None)]
-input_rows = list(zip(input_cols[0],
-                      input_cols[1],
-                      input_cols[2],
-                      input_cols[6],
-                      input_cols[7],
-                      input_cols[9],
-                      input_cols[12],
-                      input_cols[13]
-                      ))
+def get_input():
+    input_cols = xlsx_read(sheet_name="男（799）")
+    # a exp line in total_rows: (1, '骨科疾病', '骨关节炎', 0.609499828501506, 8.868, 14.5497, 2, None)]
+    input_rows = list(zip(input_cols[0],
+                          input_cols[1],
+                          input_cols[2],
+                          input_cols[6],
+                          input_cols[7],
+                          input_cols[9],
+                          input_cols[12],
+                          input_cols[13]
+                          ))
+    return input_cols, input_rows
 
-# ================= #
-# step1: selecting results
-# ================= #
-"""
-内分泌系统疾病
-妇科疾病
-循环系统疾病
-泌尿系统疾病
-消化系统疾病
-神经系统疾病
-肿瘤
-骨科疾病
 
-"""
-jibing_high_p = [i for i in input_rows
-                 if i[0] == 1 and i[4] >= 10]
-jibing_high_population = [i for i in input_rows
-                          if i[0] == 1 and i[3] >= 2 and i[4] <= 10]
-jibing = jibing_high_p + jibing_high_population
+def selecting_results():
+    """
+    从所有检测项目中挑出我们关注的项
+    :return:
+    """
+    input_cols, input_rows = get_input()
 
-kuangwz = [i for i in input_rows
-           if i[1] == "矿物质" and "高" in i[-1]]
+    jibing_high_p = [i for i in input_rows
+                     if i[0] == 1 and i[4] >= 10]
+    jibing_high_population = [i for i in input_rows
+                              if i[0] == 1 and i[3] >= 2 and i[4] <= 10]
+    jibing = jibing_high_p + jibing_high_population
 
-weiss = [i[2] for i in input_rows
-         if i[1] == "维生素" and i[2] != "叶酸" and "高" in i[-1]]
+    kuangwz = [i for i in input_rows
+               if i[1] == "矿物质" and "高" in i[-1]]
 
-yesuan = [i[2] for i in input_rows
-          if i[2] == "叶酸" and "高" in i[-1]]
+    weiss = [i[2] for i in input_rows
+             if i[1] == "维生素" and i[2] != "叶酸" and "高" in i[-1]]
 
-shansdx = [i for i in input_rows
-           if i[1] == "膳食代谢" and ("高" in i[-1] or "浅尝" in i[-1] or "戒酒" in i[-1] or "小酌" in i[-1] or "不耐受" in i[-1])]
+    yesuan = [i[2] for i in input_rows
+              if i[2] == "叶酸" and "高" in i[-1]]
 
-personal_features = [i for i in input_rows
-                     if i[0] == 3]
+    shansdx = [i for i in input_rows
+               if i[1] == "膳食代谢" and ("高" in i[-1] or "浅尝" in i[-1] or "戒酒" in i[-1] or "小酌" in i[-1] or "不耐受" in i[-1])]
 
-"""
+    personal_features = [i for i in input_rows
+                         if i[0] == 3]
+
+    return jibing, kuangwz, weiss, yesuan, shansdx, personal_features
+
+
 # ========================= #
-# step2: advices
+# advices
 # ========================= #
+jibing, kuangwz, weiss, yesuan, shansdx, personal_features =  selecting_results()
 diet_advices = []
 
 # 矿物质相关建议
@@ -148,75 +144,100 @@ personal_sport_features = []
 for i in personal_features:
     if i[1] == "运动能力":
         personal_sport_features.append(i)
-"""
 
-"""
+
 # ====================== #
-# knowledge
+# step3: load knowledge
 # ====================== #
-knowledge_cols = xlsx_read(file_dir=Params.advices_relation, sheet_name="man_799", max_col=3, min_row=1, max_row=1000)
-knowledge_rows = list(zip(knowledge_cols[0], knowledge_cols[1]))
-for row in knowledge_rows:
-    if row[0] == 0 and ("烟" not in row[1] and "酒" not in row[1]):
-        print(row)
+def knowledge():
+    """
+    :return: original_advices; splited_advices; splited_advices_v2; splited_advices_descriptions
+    """
+    advices_cols = xlsx_read(Params.man_and_woman_799_advices, sheet_name="man_and_woman_799", max_col=3, min_row=1, max_row=1000)
+    knowledge_rows = list(zip(advices_cols[1], advices_cols[2]))
+    cnt = 0
+    cnt2 = 0
+    diseases = []
+    diseases2 = []
+    original_advices = dict()  # 原始建议
+    splited_advices = dict()  # 分词结果
+    label = False
+    label2 = False
+    if knowledge_rows[-1] != ("end", "end"):
+        knowledge_rows.append(("end", "end"))
+    for row in knowledge_rows:
+        d = row[0]
+        if "#" in d:
+            temp = []
+            label = True
+            cnt = cnt + 1
+            d = d[1:]
+            d = d.replace(" ", "")
+            diseases.append(d)
+            # print(d)
+        if label and "分词结果" in row[0]:
+            # print(diseases[cnt-1])
+            label = False
+            original_advices[diseases[cnt-1]] = temp
+        if label and "(" in row[0]:
+            temp.append(row[0])
 
-for row in knowledge_rows:
-    if row[0] == 1:
-        print(row)
+        if "分词结果" in d:
+            temp2 = []
+            label2 = True
+            cnt2 = cnt2 + 1
+            continue
+            # print(d)
+        if label2 and ("#" in row[0] or "end" in row[0]):
+            # print(diseases[cnt-2])
+            label2 = False
+            splited_advices[diseases[cnt-2]] = temp2
+        if label2:
+            temp2.append((row[0], row[1]))
 
-for row in knowledge_rows:
-    if row[0] == 2:
-        print(row)
-"""
-
-advices_cols = xlsx_read(Params.man_and_woman_799_advices, sheet_name="man_799_vn", max_col=3, min_row=1, max_row=1000)
-knowledge_rows = list(zip(advices_cols[1], advices_cols[2]))
-cnt = 0
-cnt2 = 0
-diseases = []
-diseases2 = []
-original_advices = dict()
-splited_advices = dict()
-label = False
-label2 = False
-if knowledge_rows[-1] != ("end", "end"):
-    knowledge_rows.append(("end", "end"))
-for row in knowledge_rows:
-    d = row[0]
-    if "#" in d:
+    # 建议描述
+    splited_advices_descriptions = []
+    for key in splited_advices.keys():
+        for row in splited_advices[key]:
+            splited_advices_descriptions.append(row[1])
+            # print(row[1])
+    splited_advices_descriptions = set(splited_advices_descriptions)  # 去处重复
+    splited_advices_v2 = dict()
+    for row in splited_advices_descriptions:
         temp = []
-        label = True
-        cnt = cnt + 1
-        d = d[1:]
-        d = d.replace(" ", "")
-        diseases.append(d)
-        # print(d)
-    if label and "分词结果" in row[0]:
-        print(diseases[cnt-1])
-        label = False
-        original_advices[diseases[cnt-1]] = temp
-    if label and "(" in row[0]:
-        temp.append(row[0])
-
-    if "分词结果" in d:
-        temp2 = []
-        label2 = True
-        cnt2 = cnt2 + 1
-        continue
-        # print(d)
-    if label2 and ("#" in row[0] or "end" in row[0]):
-        print(diseases[cnt-2])
-        label2 = False
-        splited_advices[diseases[cnt-2]] = temp2
-    if label2:
-        temp2.append(row[0])
+        for key in splited_advices.keys():
+            for row1 in splited_advices[key]:
+                if row1[1] == row:
+                    temp.append(row1[0])
+        splited_advices_v2[row] = temp
+    return original_advices, splited_advices, splited_advices_v2, splited_advices_descriptions
 
 
-# advices
+original_advices, splited_advices, splited_advices_v2, _ = knowledge()
+
+# ================= #
+# output advices
+# ================= #
+output_advices = []
 for row in jibing:
     print(row[2], ":")
     for advice in splited_advices[row[2]]:
-        print("    "+advice)
+        output_advices.append(advice)
+        print("    ", advice[0])
 
+# merging output advices
+final_output_advices = []
+for row in output_advices:
+    if "s" in row[1]:
+        final_output_advices.append(row)
+        output_advices.remove(row)
+idx = [i[1] for i in output_advices]
+idx = set(idx)
+for row in idx:
+    advice = splited_advices_v2[row][0]
+    final_output_advices.append(advice)
+
+# for row in diet_advices:
+#     print(row)
 
 
